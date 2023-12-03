c19bbebb98e4d0026dffdce8d70b14f4ad2f3a2d
Roberto Villegas-Diaz <bthillo@hotmail.com>
2023-12-03 16:12:32 +0000

    Update test descriptions, day 2

--- tests/testthat/test-day02.R
+++ tests/testthat/test-day02.R
@@ -1,10 +1,10 @@
-test_that("day 012 - part a function achieves expected result", {
+test_that("day 02 - part a function achieves expected result", {
   x <- load_test_data("02a")
 
   expect_equal(f02a(x), 8)
 })
 
-test_that("day 012 - part a function achieves expected result", {
+test_that("day 02 - part b function achieves expected result", {
   x <- load_test_data("02a") # same as in part a
 
   expect_equal(f02b(x), 2286)
