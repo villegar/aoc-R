161075196a13be22c6fe5bcfd51bcbdcecc389a2
Roberto Villegas-Diaz <bthillo@hotmail.com>
2023-12-17 14:13:10 +0000

    Add tests for day 10

--- tests/testthat/test-day10.R
+++ tests/testthat/test-day10.R
@@ -1,3 +1,13 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("day 10 - part 1 function achieves expected result", {
+  x <- load_test_data("10a")
+
+  expect_equal(f10a(x), 4)
 })
+
+test_that("day 10 - part 2 function achieves expected result", {
+  x <- load_test_data("10b")
+
+  expect_equal(f10b(x), 4)
+})
+
+
